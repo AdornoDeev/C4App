ec94ff0287900ef0e7e58d5369e3244d5b533790
Adorno S/wEngineer <contatoadorno11@gmail.com>
2025-05-16 20:20:18 -0300

    Funções adicionadas em modular_functions entre todo o projeto e aletração do nível do arquivo

--- backend/acess/register/pj_register/pj_registerexe.py
+++ backend/acess/register/pj_register/pj_registerexe.py
@@ -1,3 +1,9 @@
-from backend.acess.register import modular_functions as mo
-
-mo.validate_password('Jo94871212@')
+from backend import modular_functionsinlore as mo
+# mo.validate_name("Stack's house main")
+'''password = None
+while not password:
+    password = mo.validate_password(input('Digite a senha que deseja cadastrar: '))
+'''
+mail = None
+while not mail:
+    mail = mo.validate_mail(input('Digite o E-mail que deseja cadastrar: '))
