cd8b97fa93edff9be54fffaaf61fe464679e589b
Adorno S/wEngineer <contatoadorno11@gmail.com>
2025-05-16 22:32:48 -0300

    Alteração realizada

--- backend/acess/register/pj_register/pj_registerexe.py
+++ backend/acess/register/pj_register/pj_registerexe.py
@@ -1,9 +1,12 @@
 from backend import modular_functionsinlore as mo
-# mo.validate_name("Stack's house main")
 '''password = None
 while not password:
     password = mo.validate_password(input('Digite a senha que deseja cadastrar: '))
 '''
-mail = None
+'''mail = None
 while not mail:
-    mail = mo.validate_mail(input('Digite o E-mail que deseja cadastrar: '))
+    mail = mo.validate_mail(input('Digite o E-mail que deseja cadastrar: '))'''
+
+cnpj = None
+while not cnpj:
+    cnpj = mo.validate_cnpj(input('Digite os números do CNPJ sem nenhum tipo de pontuação ou traços: '))
